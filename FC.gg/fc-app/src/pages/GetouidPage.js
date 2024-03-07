047b3ed82ce87d2f263b88115317e126efb3e5f0
svng-zu <zztisdudoo@gmail.com>
2024-03-07 16:08:32 +0900

    for✨ Feat    새로운 기능 추가

--- FC.gg/fc-app/src/pages/GetouidPage.js
+++ FC.gg/fc-app/src/pages/GetouidPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 // import MatchDetail from '../components/matchdetail';
 
@@ -8,9 +8,19 @@ function GetouidPage() {
   const [nickname, setNickname] = useState('');
   const [ouid, setOuid] = useState('');
   const [matchtype, setMatch] = useState('');
-  const [matchid, setMatchid] = useState('');
   const [matchdetail, setMatchdetail] = useState(null);
-  
+  // const [myStatus, setMyStatus] = useState('');
+  // const [myScore, setMyScore] = useState('');
+  // const [myPlayerData, setMyPlayerData] = useState([]);
+  // const [myData, setMyData] = useState([]);
+  // const [otherStatus, setOtherStatus] = useState('');
+  // const [otherScore, setOtherScore] = useState('');
+  // const [otherPlayerData, setOtherPlayerData] = useState([]);
+  // const [otherData, setOtherData] = useState([]);
+  useEffect(() => {
+    sessionStorage.setItem('nickname', nickname);
+  }, [nickname]);  
+
   const handleGetouid = async () => {
     try {
       const response = await axios.get('https://p0l0evybh6.execute-api.ap-northeast-2.amazonaws.com/dev/Getouid', {
@@ -40,7 +50,7 @@ function GetouidPage() {
         }
       });
       setMatch(matchtype);
-      setMatchid(response1.data);
+
       console.log(response1);
 
       const matchData = {};
@@ -50,17 +60,19 @@ function GetouidPage() {
         console.log('id', id);
         const response2 = await axios.get('https://p0l0evybh6.execute-api.ap-northeast-2.amazonaws.com/dev/Getmatchdetail', {
           params: {
-            matchid: String(id)
+            matchid: String(id),
+            nickname: sessionStorage.getItem('nickname')
           }
         });
         // 응답 데이터를 처리하거나 상태에 저장
 
         console.log(response2.data);
         matchData[id] = response2.data;
-        setMatchdetail(matchData);
+        
+
       }));
-      
-      
+    console.log(matchData);
+    setMatchdetail(matchData)
     } catch (error) {
       console.error('Error:', error);
     }
@@ -68,31 +80,7 @@ function GetouidPage() {
 
 
   };
-  const displayMatchDetail = () => {
-    if (matchdetail) {
-      return (
-        <div>
-          {Object.keys(matchdetail).map((matchId) => (
-            <div key={matchId}>
-              <p>Match ID: {matchId}</p>
-              <p>Match Date: {matchdetail[matchId].matchDate}</p>
-              <p>Match Type: {matchdetail[matchId].matchType}</p>
-              <div>
-                Match Info:
-                {matchdetail[matchId].matchInfo.map((info, index) => (
-                  <div key={index}>
-                    <p>{`Info ${index + 1}: ${info}`}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-      );
-    } else {
-      return <p>No match detail available</p>;
-    }
-  };
+
 
 
   return (
@@ -103,18 +91,29 @@ function GetouidPage() {
       <button onClick={() => {
           handleGetouid();
       }}>검색</button>
-      {ouid && <p>OUID: {ouid}</p>}
     <div>
-      {Array.isArray(matchid) && matchid.map((item, index) => (
-        <p key={index}>{item}</p>
-      ))}
+      <h2>Match Data</h2>
+      {matchdetail && Object.keys(matchdetail).length > 0 ? (
+        Object.keys(matchdetail).map((matchId) => (
+          <div key={matchId}>
+            <p>나 : {matchdetail[matchId].my_status} 상대방 : {matchdetail[matchId].other_status} </p>
+            <p>내 점수: {matchdetail[matchId].my_score}, 상대방 점수 : {matchdetail[matchId].other_score}</p>
+            <p>내 선수 : </p>
+            <ul>
+              {matchdetail[matchId].my_data.map((player, index) => (
+                <li key={index}>
+                  <img src={player.p_action_image} alt={player.name} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))
+      ) : (
+        <p>No match data available</p>
+      )}
     </div>
-    <div>
-      <h2>Match Detail</h2>
-      {displayMatchDetail()}
-    </div>
-      
-    </div>
+
+  </div>
   );
 }
 
